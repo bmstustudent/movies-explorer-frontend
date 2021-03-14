29c73f2002959ec66ac6932911ae95be1195d33e
Shults Pavel <bmstustudent@yandex.ru>
2021-03-14 17:27:24 +0300

    fix: movies

--- src/components/Movies/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './FilterCheckbox.css';
 
 // фильтр с чекбоксом «Только короткометражки»
-function FilterCheckbox () {
+function FilterCheckbox({ shortMovie, handleShortMovieChange }) {
+  const handleCheckboxClick = () => {
+    handleShortMovieChange();
+  };
   return (
     <div className='filter-checkbox'>
       <label className='filter-checkbox__container' htmlFor='checkbox'>
-        <input className='filter-checkbox_invisible' type="checkbox" id='checkbox' value='Короткометражки'></input>
+        <input className='filter-checkbox_invisible' type="checkbox" onChange={handleCheckboxClick} checked={shortMovie} id='checkbox' value='Короткометражки'></input>
         <span className="filter-checkbox_visible"></span>
         <span className='filter-checkbox_text'>Короткометражки</span>
       </label>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,15 +1,76 @@
-import React from 'react';
 import './Movies.css';
-
+import Header from '../Header/Header';
 import SearchForm from './SearchForm/SearchForm';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
+import { useEffect, useState } from 'react';
+import * as helper from '../../../utils/helpers';
 
 // компонент страницы с поиском по фильмам
-function Movies () {
+function Movies(
+  allMovies,
+  onMoviesSearchSubmit,
+  isDisabledSearch,
+  onClickCardButton,
+  isVisiblePreloader,
+  messageNoMovies
+) {
+  const [searchValue, setSearchValue] = useState('');
+  const [isCheckboxChecked, setCheckboxChecked] = useState(false);
+  const [moviesCount, setMoviesCount] = useState(helper.getMoviesCount());
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [currentMovies, setCurrentMovies] = useState([]);
+
+  function handleSearchSubmit(value) {
+    setSearchValue(value);
+    if (!allMovies.length) {
+      onMoviesSearchSubmit();
+    }
+  }
+
+  function handleCheckboxChange() {
+    setCheckboxChecked(!isCheckboxChecked);
+  }
+
+  function handleClickMoreButton() {
+    setMoviesCount(moviesCount + helper.getAddMoviesCount());
+  }
+
+  useEffect(() => {
+    const moviesFound = helper.searchFilter(allMovies, searchValue);
+    const moviesFiltered = helper.durationFilter(moviesFound, isCheckboxChecked);
+    setFilteredMovies(moviesFiltered);
+    setCurrentMovies(moviesFiltered.slice(0, moviesCount));
+  }, [allMovies, searchValue, isCheckboxChecked, moviesCount]);
+
+  useEffect(() => {
+    function updateCardsList() {
+      setTimeout(() => {
+        setMoviesCount(helper.getMoviesCount());
+        setCurrentMovies(filteredMovies.slice(0, helper.getMoviesCount()));
+      }, 700);
+    }
+    window.addEventListener('resize', updateCardsList);
+    return () => window.removeEventListener('resize', updateCardsList);
+  }, [filteredMovies]);
   return (
     <section className='movies'>
-      <SearchForm/>
-      <MoviesCardList/>
+      <Header />
+      <SearchForm
+        onSearchSubmit={handleSearchSubmit}
+        isDisabledSearch={isDisabledSearch}
+        isCheckboxChecked={isCheckboxChecked}
+        onCheckboxChange={handleCheckboxChange}
+      />
+      <MoviesCardList
+        cards={currentMovies}
+        messageNoMovies={messageNoMovies}
+        isVisiblePreloader={isVisiblePreloader}
+        isVisibleButtonMore={filteredMovies.length > currentMovies.length}
+        onClickMoreButton={handleClickMoreButton}
+        onClickCardButton={onClickCardButton}
+      />
+      <Footer />
     </section>
   )
 }
