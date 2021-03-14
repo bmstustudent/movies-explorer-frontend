1a3b71eef89896c1397dd28425488e097b19cbdd
Shults Pavel <bmstustudent@yandex.ru>
2021-03-14 17:04:08 +0300

    fix: MoviesCardList

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,24 +1,48 @@
-import React from 'react';
 import './MoviesCardList.css';
-
+import Preloader from '../../Preloader/Preloader';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 // компонент, который управляет отрисовкой карточек фильмов на страницу и их количеством
-function MoviesCardList () {
+function MoviesCardList(
+  cards,
+  messageNoMovies,
+  isVisiblePreloader,
+  isVisibleButtonMore,
+  onClickMoreButton,
+  onClickCardButton
+) {
+  const isVisibleMessage = !cards.length;
+  const isVisibleCards = !!cards.length;
   return (
-    <div className='movies-card-list'>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
+    <div className='cards'>
+      {
+        isVisibleMessage &&
+        <p className="cards__message">{messageNoMovies}</p>
+      }
+      {
+        isVisibleCards &&
+        <ul className="cards__list">
+          {cards.map((card) => (
+            <MoviesCard
+              key={card._id}
+              card={card}
+              onClickCardButton={onClickCardButton}
+            />
+          ))}
+        </ul>
+      }
+      {
+        isVisibleButtonMore &&
+        <button
+          className="cards__button"
+          type="button"
+          name="more"
+          onClick={onClickMoreButton}
+        >
+          Ещё
+        </button>
+      }
+      <Preloader isVisiblePreloader={isVisiblePreloader} />
     </div>
   )
 }
